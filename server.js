0124361c8048db791872523115bcc6c72a6950fa
viveksingh045 <vivekkumarsingh9450@gmail.com>
2025-09-27 23:18:28 +0530

    feat: set up Express server and add image upload form in index.ejs

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
-
+import express from 'express';
 import dotenv from 'dotenv';
 dotenv.config();
 import mongoose from 'mongoose';
 
-const uri = process.env.MONGODB_URI;
 
+const app = express();
+const port = process.env.PORT || 1000;
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
+
+
+// MongoDB connection URI
+const uri = process.env.MONGODB_URI;
 async function main() {
-  try {
-    // Connect (Mongoose handles recommended options internally)
-    await mongoose.connect(uri);    // returns a promise
+  try {   
+    await mongoose.connect(uri);    
     console.log('✅ Connected to MongoDB Atlas');
 
   } catch (err) {
@@ -18,3 +26,9 @@ async function main() {
 }
 
 main();
+
+
+//rendering engine
+app.get('/', (req, res) => {
+    res.render('index.ejs', { url:null});
+});
